19da2560c5d956399bff3229f8b19a78ed11d9d7
casperfibaek <casperfibaek@gmail.com>
2021-10-25 15:56:05 +0200

    no_auth for test

--- routes/auth.ts
+++ routes/auth.ts
@@ -10,29 +10,33 @@ import { Request, Response } from 'express';
 import credentials from './credentials';
 
 export default async function auth(req:Request, res:Response, next:Function): Promise<void> {
-  try {
-    if (req.query.username === 'casper' && req.query.token === 'golden_ticket') {
-      next();
-    } else if (req.query.username === 'dss' && req.query.token === 'golden_ticket') {
-      next();
-    } else {
-      const userId = req.headers.authorization.split(':')[0];
-      const token = req.headers.authorization.split(':')[1];
-      const decodedToken:any = jwt.verify(token, credentials.admin_key);
-
-      if (userId === decodedToken.userId) {
-        next();
-      } else {
-        res.status(401).json({
-          status: 'Error',
-          message: 'User Unauthorised.',
-        });
-      }
-    }
-  } catch {
-    res.status(401).json({
-      status: 'Error',
-      message: 'User unauthorised or unable to read token.',
-    });
-  }
+  next();
 }
+
+// export default async function auth(req:Request, res:Response, next:Function): Promise<void> {
+//   try {
+//     if (req.query.username === 'casper' && req.query.token === 'golden_ticket') {
+//       next();
+//     } else if (req.query.username === 'dss' && req.query.token === 'golden_ticket') {
+//       next();
+//     } else {
+//       const userId = req.headers.authorization.split(':')[0];
+//       const token = req.headers.authorization.split(':')[1];
+//       const decodedToken:any = jwt.verify(token, credentials.admin_key);
+
+//       if (userId === decodedToken.userId) {
+//         next();
+//       } else {
+//         res.status(401).json({
+//           status: 'Error',
+//           message: 'User Unauthorised.',
+//         });
+//       }
+//     }
+//   } catch {
+//     res.status(401).json({
+//       status: 'Error',
+//       message: 'User unauthorised or unable to read token.',
+//     });
+//   }
+// }
