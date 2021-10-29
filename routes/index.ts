92e73152aa5b65fe605613006b408ec25a89cc91
AnaCMFernandes <55448524+AnaCMFernandes@users.noreply.github.com>
2021-10-29 16:01:54 +0200

    fix get_forecast function and update packages

--- routes/index.ts
+++ routes/index.ts
@@ -13,7 +13,7 @@ import Wfw from '../assets/whatfreewords';
 import Pluscodes from '../assets/pluscodes';
 import { callbackify } from 'util';
 
-const axios = require("axios");
+import axios from "axios"
 
 const version = '0.2.2';
 
